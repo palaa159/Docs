a775c41efb177228f64354209305359ca0fca23d
Richard Waltman <retiutut@icloud.com>
2019-07-27 20:00:47 -0500

    add focus+motor arduino example

--- assets/files/OpenBCI_GUI-FocusArduinoWithMotor/OpenBCI_GUI-FocusArduinoWithMotor.ino
+++ assets/files/OpenBCI_GUI-FocusArduinoWithMotor/OpenBCI_GUI-FocusArduinoWithMotor.ino
@@ -1,17 +1,29 @@
 ////////////////////////////////////////////////////////////////////////
-//            OpenBCI_GUI to Arduino via Serial: Focus                //
+//        OpenBCI_GUI to Arduino via Serial: Focus w/ Motor           //
 //                                                                    //
 //    - The Arduino Built-In LED blinks when the user is Focused      //
 //   https://docs.openbci.com/Tutorials/17-Arduino_Focus_Example.md   //
-//   Tested 4/7/2019 using iMac, Genuine Arduino, OpenBCI_GUI 4.1.2   //
+//   Tested 7/27/2019 using iMac, Genuine Arduino, OpenBCI_GUI 4.1.3  //
 ////////////////////////////////////////////////////////////////////////
 
 const byte numChars = 32;
 char receivedChars[numChars];   // an array to store the received data
 String previousData = "";
 boolean newData = false;
+boolean isFocused = false;
+
+int enablePin = 11;
+int in1Pin = 10;
+int in2Pin = 9;
+int switchPin = 7;
+int potPin = 0;
 
 void setup() {
+    pinMode(in1Pin, OUTPUT);
+    pinMode(in2Pin, OUTPUT);
+    pinMode(enablePin, OUTPUT);
+    pinMode(switchPin, INPUT_PULLUP);
+
     Serial.begin(57600);
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.println("<Arduino is ready>");
@@ -20,6 +32,14 @@ void setup() {
 void loop() {
     recvWithEndMarker();
     showNewData();
+    
+    int speed = analogRead(potPin) / 4;
+    boolean reverse = digitalRead(switchPin);
+    if (isFocused) {
+      setMotor(speed, reverse);
+    } else {
+      setMotor(0, reverse);
+    }
 }
 
 //Recieve data and look for the endMarker '\n' (new line)
@@ -55,10 +75,12 @@ void showNewData() {
           //Check if the string is "true" or "false"
           if (s.equals("false")) {
             Serial.println("Input: FALSE");
+            isFocused = false;
             digitalWrite(LED_BUILTIN, LOW);
           } else if (s.equals("true")) {
             Serial.println("Input: TRUE");
             digitalWrite(LED_BUILTIN, HIGH);
+            isFocused = true;
           } else {
             //Otherwise print the incoming with no action
             Serial.println("This just in ... " + s);
@@ -69,3 +91,8 @@ void showNewData() {
     }
 }
 
+void setMotor(int speed, boolean reverse) {
+    analogWrite(enablePin, speed);
+    digitalWrite(in1Pin, !reverse);
+    digitalWrite(in2Pin, reverse);
+}
